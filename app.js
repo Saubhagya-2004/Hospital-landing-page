8aec536b7f66c8c53df2b9064c67b9fe923dd0fc
Saubhagya Baliarsingh <saubhagyabaliarsingh2@gmail.com>
2024-12-24 01:42:39 +0530

    changes

--- app.js
+++ app.js
@@ -2,5 +2,15 @@ $(document).ready(function(){
     $('.fa-bars').click(function(){
         $(this).toggleClass('fa-times');
         $('.navbar').toggleClass('nav-links');
+    });
+    $(window).on('scroll load',function(){
+        $('.fa-bars').removeClass('fa-times');
+        $('.navbar').removeClass('nav-links');
+        if($(window).scrollTop()>30){
+            $('header').addClass('header-active');
+        }
+        else{
+            $('header').removeClass('header-active');
+        }
     })
 })
